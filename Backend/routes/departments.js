99dcb33497a377f9dbb10f5bd587532a833a2260
hamzaiqbal2000 <hiqbal.bscs18seecs@seecs.edu.pk>
2021-08-04 12:17:49 +0500

    fixed some bugs and add the react boiler-plate

--- Backend/routes/departments.js
+++ Backend/routes/departments.js
@@ -1,94 +1,105 @@
-const { validate, Department } = require('../models/department');
-const express = require('router');
-const { User } = require('../models/user');
+const { validate, Department } = require("../models/department");
+const express = require("express");
+const { User } = require("../models/user");
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    const departments = await Department.find();
-    res.send(departments);
+router.get("/", async (req, res) => {
+  const departments = await Department.find();
+  res.send(departments);
 });
 
-router.get('/:id', async (req, res) => {
-    const department = await Department.findById(req.params.id);
+router.get("/:id", async (req, res) => {
+  const department = await Department.findById(req.params.id);
 
-    if(!department){
-        return res.status(404).send("The department you have search for is not available")
-    }
-    res.send(department);
-})
+  if (!department) {
+    return res
+      .status(404)
+      .send("The department you have search for is not available");
+  }
+  res.send(department);
+});
 
-router.post('/', async (req, res) => {
-    const { error } = validate(req.body);
-    if(error){
-        return res.status(400).send(error.details[0].message);
-    }
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
 
-    const user = await User.findById(req.body.userId);
-    if(!user) return res.status(400).send("Invalid user...");
+  const user = await User.findById(req.body.userId);
+  if (!user) return res.status(400).send("Invalid user...");
 
-    //creating a new department
-    const department = new Department({
-        name: req.body.title,
-        teams: [{
-            people:[{
-                _id: user._id,
-                name: user.name
-            }],
-            teamLead:{
-                _id: user._id,
-                name: user.name
-            }
-        }],
-        inCharge: {
+  //creating a new department
+  const department = new Department({
+    name: req.body.title,
+    teams: [
+      {
+        people: [
+          {
             _id: user._id,
-            name: user.name
-        }
-    });
+            name: user.name,
+          },
+        ],
+        teamLead: {
+          _id: user._id,
+          name: user.name,
+        },
+      },
+    ],
+    inCharge: {
+      _id: user._id,
+      name: user.name,
+    },
+  });
 
-    await department.save();
-    res.send(department);
-
+  await department.save();
+  res.send(department);
 });
 
-router.put('/:id'), async (req, res) => {
+router.put("/:id"),
+  async (req, res) => {
     const { error } = validate(req.body);
-    if(error){
-        return res.status(400).send(error.details[0].message);
+    if (error) {
+      return res.status(400).send(error.details[0].message);
     }
 
     const user = await User.findById(req.body.userId);
-    if(!user) return res.status(400).send("Invalid user...");
+    if (!user) return res.status(400).send("Invalid user...");
 
     let department = await Department.findByIdAndUpdate(
-        req.params.id,
-        {
-            name: req.body.title,
-            teams: [{
-                people:[{
-                    _id: user._id,
-                    name: user.name
-                }],
-                teamLead:{
-                    _id: user._id,
-                    name: user.name
-                }
-            }],
-            inCharge: {
-            _id: user._id,
-            name: user.name
-            }
-        }, 
-        {
-            new: true
-        }
-        );
+      req.params.id,
+      {
+        name: req.body.title,
+        teams: [
+          {
+            people: [
+              {
+                _id: user._id,
+                name: user.name,
+              },
+            ],
+            teamLead: {
+              _id: user._id,
+              name: user.name,
+            },
+          },
+        ],
+        inCharge: {
+          _id: user._id,
+          name: user.name,
+        },
+      },
+      {
+        new: true,
+      }
+    );
 
-        if(!department){
-            return res.status(404).send("The department you are trying to find is not available")
-        }
+    if (!department) {
+      return res
+        .status(404)
+        .send("The department you are trying to find is not available");
+    }
 
-        res.send(department);
-
-}
+    res.send(department);
+  };
 
 module.exports = router;
--- Backend/startup/routes.js
+++ Backend/startup/routes.js
@@ -1,13 +1,11 @@
-const departments = require('../routes/departments');
-const teams = require('../routes/teams');
-const users = require('../routes/users');
-const express = require('express');
+const departments = require("../routes/departments");
+const teams = require("../routes/teams");
+const users = require("../routes/users");
+const express = require("express");
 
-
-
-module.exports = function (app){
-    app.use(express.json());
-    app.use('/api/departments', departments);
-    app.use('/api/teams', teams);
-    app.use('/api/users', users);
-}
+module.exports = function (app) {
+  app.use(express.json());
+  app.use("/api/departments", departments);
+  app.use("/api/teams", teams);
+  app.use("/api/users", users);
+};
